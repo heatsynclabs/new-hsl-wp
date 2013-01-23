c0df9911401fbdc8b94dc74ba46dc6c7369e5036
Jacob Rosenthal <jakerosenthal@gmail.com>
2013-01-23 02:44:32 -0700

    untracked changes

--- category.php
+++ category.php
@@ -1,4 +1,18 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
 
                 <section id="primary">
                         <div id="content" role="main">
@@ -53,5 +67,4 @@ leven' ); ?></p>
                         </div><!-- #content -->
                 </section><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
